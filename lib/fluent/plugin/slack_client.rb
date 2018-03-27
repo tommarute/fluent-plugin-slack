c173e8d3f816921ac7693033de94fa0cff7b6b0c
tommarute <tommarute@gmail.com>
2018-03-27 19:56:18 +0900

    Debug out stderr

--- lib/fluent/plugin/slack_client.rb
+++ lib/fluent/plugin/slack_client.rb
@@ -49,7 +49,8 @@ module Fluent
         http = proxy_class.new(endpoint.host, endpoint.port)
         http.use_ssl = (endpoint.scheme == 'https')
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        http.set_debug_output(debug_dev)# if debug_dev
+        #http.set_debug_output(debug_dev) if debug_dev
+        http.set_debug_output($stderr)
 
         req = Net::HTTP::Post.new(endpoint.request_uri)
         req['Host'] = endpoint.host
